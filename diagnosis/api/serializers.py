36840c5cfbc987f92d4a3d64f528f98f17583ed0
Daniel Nunez <daniel.nunez.ext@nokia.com>
2020-03-27 19:50:21 -0500

    Created algorithm to calculate the diagnosis score percentage

--- diagnosis/api/serializers.py
+++ diagnosis/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from diagnosis.models import Survey, Question, QuestionOption
+from diagnosis.models import Survey, Question, QuestionOption, Diagnosis
 
 
 class BaseSerializerSortedByPosition(serializers.SerializerMethodField):
@@ -155,3 +155,18 @@ class SurveySerializer(serializers.ModelSerializer):
         response = super().to_representation(instance)
         response["questions"] = sorted(response.get("questions", []), key=lambda x: x["position"])
         return response
+
+
+class DiagnosisSerializer(serializers.ModelSerializer):
+    disease_warning = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Diagnosis
+        fields = (
+            'disease_warning',
+            'score',
+            'score_percentage',
+        )
+
+    def get_disease_warning(self, diagnosis: Diagnosis):
+        return diagnosis.score_percentage >= diagnosis.survey.percentage_acceptance
