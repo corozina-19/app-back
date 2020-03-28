2c25f500b618da3150348199c55c4588a4fbdd50
jrnp97 <jrnp1997@gmail.com>
2020-03-27 23:24:33 -0300

    Finish test and view logic

--- chat/api/viewsets.py
+++ chat/api/viewsets.py
@@ -2,15 +2,36 @@
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
+from rest_framework.parsers import JSONParser
+
+from rest_framework.exceptions import ParseError
+
 from rest_framework.permissions import IsAuthenticated
 
+from chat.api.serializers import MessageSerializer
+
+from chat.models import Thread
 from chat.models import Message
 
 
 class MessageViewSet(CreateModelMixin, GenericViewSet):
     """ Message ViewSet only manage message creation is not allowed to do more """
     model = Message
+    serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [JSONParser]
 
-    class Meta:
-        model = Message
+    def create(self, request, *args, **kwargs):
+        """ Override standard method to insert thread creation logic """
+        values = list(request.data.values())
+        if not any(values) or 'thread' not in request.data:
+            raise ParseError(detail='Bad formed message information.')
+
+        if not request.data['thread']:
+            thread = Thread.objects.create(
+                patient=request.user,
+            )
+            request.data['thread'] = thread.id
+        request.data['user'] = request.user.id
+        return super().create(request, *args, **kwargs)
+
--- chat/tests/test_viewsets.py
+++ chat/tests/test_viewsets.py
@@ -2,49 +2,65 @@
 from django.test import TestCase
 from django.conf import settings
 
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
+
+User = get_user_model()
 
 
 class TestMessageViewSets(TestCase):
     """ Test Case for testing message viewsets """
     def setUp(self) -> None:
         self.main_uri = f'/api/{settings.CURRENT_API_VERSION}/chat/message/'
+        self.cred = {
+            'username': 'test_user',
+            'password': '12345'
+        }
+        self.user = User.objects.create_user(**self.cred)
 
     def test_permission_on_message_creation(self):
-        self.client.logout()
+        """ Test for proof auth validation and method validations """
         dummy_body = {
-            'thread_id': '',
-            'message_body': '',
+            'thread': '',
+            'body': '',
         }
         response = self.client.post(self.main_uri, data=dummy_body)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        logged = self.client.login(**self.cred)
+        self.assertTrue(logged)
+
         response = self.client.get(self.main_uri)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_message_creation(self):
         """ Test message creation EndPoint """
-        logged = self.client.force_login('test_user')
+        logged = self.client.login(**self.cred)
         self.assertEqual(logged, True)
 
         request_body = {
-            'thread_id': '',
-            'message_body': '',
+            'thread': '',
+            'body': '',
         }
         response = self.client.post(self.main_uri, data=request_body)
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-        request_body['message_body'] = 'Hi!'
-        response = self.client.post(self.main_uri, data=request_body)
+        response = self.client.post(self.main_uri, data=request_body, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        request_body['body'] = 'Hi!'
+        response = self.client.post(self.main_uri, data=request_body, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         data = response.json()
-        self.assertIn(data, 'thread_id')
-        self.assertIsInstance(data['thread_id'], int)
+        self.assertTrue('thread' in data)
+        self.assertIsInstance(data['thread'], int)
 
-        request_body['thread_id'] = data['thread_id']
-        response = self.client.post(self.main_uri, data=request_body)
+        request_body['thread'] = data['thread']
+        response = self.client.post(self.main_uri, data=request_body, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         data = response.json()
-        thread_id = data['thread_id']
-        self.assertEqual(thread_id, request_body['thread_id'])
+        thread_id = data['thread']
+        self.assertEqual(thread_id, request_body['thread'])
 
