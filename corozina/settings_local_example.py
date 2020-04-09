fab5b7ad5c5f174b653ae24df7c523dcf1a53de5
Rafael barboza <rafael.1.barboza.ext@nokia.com>
2020-04-09 08:56:18 -0500

    removing permissions for configuration defects for django rest

--- corozina/settings_local_example.py
+++ corozina/settings_local_example.py
@@ -38,8 +38,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.IsAdminUser'
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': (
